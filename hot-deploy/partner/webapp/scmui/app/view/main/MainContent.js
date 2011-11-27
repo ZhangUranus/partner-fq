d4b13a4519ecb8663a10ffd8cf434660e04bbd71
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2011-11-27 06:39:48 +0000

    重构树形菜单、tabpanel的代码，增加tab右键工具菜单。

--- hot-deploy/partner/webapp/scmui/app/view/main/MainContent.js
+++ hot-deploy/partner/webapp/scmui/app/view/main/MainContent.js
@@ -4,19 +4,55 @@
  * @Date 2011-11-24
  */
 Ext.define('SCM.view.main.MainContent' ,{
-		extend: 'Ext.tab.Panel',
-		alias : 'widget.maincontent',
-		id : 'main-content',
-		items : [
-			{
-				xtype: 'welcomeindex',
-				title: LocaleLang.home,
-		        iconCls: 'main-index',
-		        closable: false
+	extend: 'Ext.tab.Panel',
+	alias : 'widget.maincontent',
+	id : 'main-content',
+	tabs : [],
+	plugins: Ext.create('Ext.ux.TabCloseMenu', {}),
+	items : [
+		{
+			xtype: 'welcomeindex',
+			title: LocaleLang.home,
+	        iconCls: 'main-index',
+	        closable: false
+		}
+	],
+	initComponent: function(){
+		this.callParent(arguments);
+	},
+	hasTab : function(data) {
+		return Ext.Array.contains(this.tabs, data.id);
+	},
+	addTab : function(data){
+		if(data.leaf){
+			if(!this.hasTab(data)){
+				try {
+					this.add({
+						xtype: data.hyperlink,
+						id : data.id,
+						title: data.text,
+						iconCls: data.iconCls,
+						closable: true
+					}).show();
+				} catch (e) {
+					this.add({
+						xtype: 'pageerror',
+						id : data.id,
+						title: data.text,
+						iconCls: data.iconCls,
+						closable: true
+					}).show();
+				}
+				this.tabs.push(data.id);
+			}else{
+				this.setActiveTab(data.id);
 			}
-		],
-		initComponent: function(){
-			this.callParent(arguments);
+		}
+	},
+	removeTab : function(data){
+		var index = Ext.Array.indexOf(this.tabs, data.id);
+		if (index !== false) {
+			Ext.Array.erase(this.tabs, index, 1)
 		}
 	}
-);
+});
