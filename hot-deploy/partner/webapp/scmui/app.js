85ac9e6c59ec8735b6db91c69cd1ba2e62b7e40e
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2011-11-27 12:07:27 +0000

    增加注释。

--- hot-deploy/partner/webapp/scmui/app.js
+++ hot-deploy/partner/webapp/scmui/app.js
@@ -1,37 +1,42 @@
+/**
+ * @Purpose 系统框架类
+ * @author jeff-liu
+ * @Date 2011-11-24
+ */
 Ext.Loader.setConfig({enabled:true});
 Ext.application({
 	name:'SCM',
 	appFolder:'app',
-	controllers: [
+	controllers: [//载入系统controller
 		'Homes',
 		'basedata.UnitController'
 	],
-	launch : function(){
+	launch : function(){//初始化系统主页面
 		var viewport = Ext.create('Ext.container.Viewport',{
 			id: 'main-view',
 			layout: 'border',
 			defaults : {
 				xtype : 'container'
 			},
-			listeners: {
+			listeners: {//监控 改变浏览器大小 事件，调用重新计算布局，刷新布局。
 				resize : function() {
 					this.doLayout();
 				}
 			},
-			items: [{
+			items: [{//系统logo版面
 				region : 'north',
 				xtype : 'maintop'
 			},{
 				region : 'center',
 				layout : 'border',
 				minWidth : 800,
-				items : [{
+				items : [{//系统树形菜单版面
 					region : 'west',
 					xtype : 'maintreepanel',
 					id : 'main-tree',
 					border : 1,
 					width : 240
-				}, {
+				}, {//系统页面展示版面
 					region : 'center',
 					id : 'main-content-container',
 					layout : 'fit',
@@ -41,13 +46,13 @@ Ext.application({
 						xtype : 'maincontent'
 					}
 				}]
-			},{
+			},{//系统页脚
 				region : 'south',
 				id : 'footer',
 				height : 20,
 				contentEl : "footer-content"
 			}]
 		});
-		viewport.doLayout();
+		viewport.doLayout();  //刷新布局
 	}
 });
