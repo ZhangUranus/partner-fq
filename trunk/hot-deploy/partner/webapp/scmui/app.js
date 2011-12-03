3d77a61fa665d9344b2a03fc2b67806c0c97f3c3
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2011-12-03 00:26:37 +0000

    重构代码。

--- trunk/hot-deploy/partner/webapp/scmui/app.js
+++ trunk/hot-deploy/partner/webapp/scmui/app.js
@@ -3,60 +3,20 @@
  * @author jeff-liu
  * @Date 2011-11-24
  */
-Ext.Loader.setConfig({enabled:true});
+Ext.Loader.setConfig({enabled : true});
 Ext.application({
-	name:'SCM',
-	appFolder:'app',
-	controllers: [//载入系统controller
-		'Homes',
-		'basedata.UnitController',
-		'basedata.WarehouseTypeController',
-		'basedata.WarehouseController',
-		'basedata.CustomerController',
-		'system.SystemController'
-	],
-	launch : function(){//初始化系统主页面
-		var viewport = Ext.create('Ext.container.Viewport',{
-			id: 'main-view',
-			layout: 'border',
-			defaults : {
-				xtype : 'container'
-			},
-			listeners: {//监控 改变浏览器大小 事件，调用重新计算布局，刷新布局。
-				resize : function() {
-					this.doLayout();
-				}
-			},
-			items: [{//系统logo版面
-				region : 'north',
-				xtype : 'maintop'
-			},{
-				region : 'center',
-				layout : 'border',
-				minWidth : 800,
-				items : [{//系统树形菜单版面
-					region : 'west',
-					xtype : 'maintreepanel',
-					id : 'main-tree',
-					border : 1,
-					width : 240
-				}, {//系统页面展示版面
-					region : 'center',
-					id : 'main-content-container',
-					layout : 'fit',
-					minWidth : 800,
-					border : false,
-					items : {
-						xtype : 'maincontent'
-					}
-				}]
-			},{//系统页脚
-				region : 'south',
-				id : 'footer',
-				height : 20,
-				contentEl : "footer-content"
-			}]
-		});
-		viewport.doLayout();  //刷新布局
-	}
+    name : 'SCM',
+    appFolder : 'app',
+    controllers : [// 载入系统controller
+        'Main', 
+        'basedata.UnitController', 
+        'basedata.WarehouseTypeController', 
+        'basedata.WarehouseController', 
+        'basedata.CustomerController', 
+        'system.SystemController'
+    ],
+    launch : function() {
+        var viewport = Ext.create('SCM.view.Viewport');
+        viewport.doLayout(); // 刷新布局
+    }
 });
