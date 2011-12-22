ba38842715f23da8c82174928b3a1fe8fffdbd05
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2011-12-22 04:48:34 +0000

    更新代码，增加用户模块

--- trunk/hot-deploy/partner/src/org/ofbiz/partner/scm/security/SecurityEvents.java
+++ trunk/hot-deploy/partner/src/org/ofbiz/partner/scm/security/SecurityEvents.java
@@ -1,6 +1,8 @@
 package org.ofbiz.partner.scm.security;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,8 @@ import javolution.util.FastList;
 
 import net.sf.json.JSONObject;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.ofbiz.base.crypto.HashCrypt;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.common.login.LoginServices;
@@ -18,6 +22,8 @@ import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.entity.condition.EntityConditionList;
 import org.ofbiz.partner.scm.common.CommonEvents;
+import org.ofbiz.partner.scm.common.TreeNode;
+import org.ofbiz.partner.scm.common.TreeOprCommon;
 
 /**
  * 用户安全事件类
@@ -86,6 +92,22 @@ public class SecurityEvents {
 		return responseString;
 	}
 	
+	public static String isLogin(HttpServletRequest request, HttpServletResponse response) {
+		JSONObject jsonStr = new JSONObject();
+		if("".equals(CommonEvents.getUsername(request))){
+			
+			jsonStr.put("success", false);
+		}else{
+			jsonStr.put("success", true);
+		}
+		try {
+			CommonEvents.writeJsonDataToExt(response, jsonStr.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "success";
+	}
+	
 	/**
      * 获取用户权限字符串(Json字符串格式)
      */
@@ -104,10 +126,99 @@ public class SecurityEvents {
 			isRoot = true;
 		}
 		try {
-			CommonEvents.writeJsonDataToExt(response, CommonEvents.getTreeDataByParentId(CommonEvents.getDelegator(request), parentId, isRoot));
+			CommonEvents.writeJsonDataToExt(response, CommonEvents.getTreeDataByParentId(request,CommonEvents.getDelegator(request), parentId, isRoot));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return "success";
 	}
+	
+	public static String getUserTreeToJson(HttpServletRequest request, HttpServletResponse response){
+		try {
+			List<GenericValue> valueList = CommonEvents.getDelegator(request).findList("Department",null, null, null, null, true);
+			List<GenericValue> userList = CommonEvents.getDelegator(request).findList("VSystemUserTree",null, null, null, null, true);
+			valueList.addAll(userList);			//将用户数据添加到list中
+			List<TreeNode> treeNodes = TreeOprCommon.buildTree(valueList); // 构建数节点
+			
+			JSONObject jsonResult=new JSONObject();
+			jsonResult.element("success", true);//成功标记
+			if(treeNodes!=null){
+				List<JSONObject> rootList=new ArrayList<JSONObject>();//根节点json对象列表
+				for(TreeNode node:treeNodes){//构建每个根节点列表
+					JSONObject rootObj=getTreeJsonObject(node);
+					if(rootObj!=null){
+						rootList.add(rootObj);
+					}
+				}
+				jsonResult.element("children", rootList);
+			}
+			
+			CommonEvents.writeJsonDataToExt(response, jsonResult.toString()); // 将结果返回前端Ext
+		} catch (GenericEntityException e) {
+			Debug.logError(e, module);
+		} catch (JsonGenerationException e) {
+			Debug.logError(e, module);
+		} catch (JsonMappingException e) {
+			Debug.logError(e, module);
+		} catch (IOException e) {
+			Debug.logError(e, module);
+		}
+		return "success";
+	}
+	
+	public static String getUserInfo(HttpServletRequest request, HttpServletResponse response) {
+		
+		return "success";
+	}
+	
+	public static JSONObject getTreeJsonObject(TreeNode treeNode){
+		if(treeNode!=null){
+			JSONObject nodeJson=new JSONObject();
+			GenericValue value=treeNode.getValue();
+			Collection<String> keys =  value.getAllKeys();
+			boolean valid = true;
+			boolean department = true;
+			for(String field : keys){
+				if("name".equals(field)){
+					nodeJson.element("text", value.getString("name"));
+				} else if("valid".equals(field)){
+					if(value.getString(field).equals("N")){
+						valid = false;
+					}
+					department = false;
+				}
+				nodeJson.element(field, value.getString(field));
+			}
+			if(department){
+				nodeJson.element("iconCls", "tree-department");
+				nodeJson.element("isUser", false);
+			}else{
+				nodeJson.element("isUser", true);
+				if(valid){
+					nodeJson.element("iconCls", "tree-user");
+				}else{
+					nodeJson.element("iconCls", "tree-user-invalid");
+				}
+			}
+			boolean isLeaf=true;//是否叶子节点
+			//递归构建children
+			List<TreeNode> childrenList=treeNode.getChildren();
+			if(childrenList!=null&&childrenList.size()>0){
+				List<JSONObject> childrenJson=new ArrayList<JSONObject>();
+				for(TreeNode child:childrenList){
+					JSONObject childJson=getTreeJsonObject(child);
+					if(childJson!=null){
+						childrenJson.add(childJson);
+					}
+				}
+				nodeJson.element("children", childrenJson);
+				isLeaf=false;
+			}
+			
+			nodeJson.element("leaf", isLeaf);
+			return nodeJson;
+		}
+		return null;
+	}
+	
 }
