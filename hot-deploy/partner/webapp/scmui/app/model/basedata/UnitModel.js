d7578856ce9facabd4419bfb3ad081a18baab40b
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-01-04 14:03:23 +0000

    使用JsonAjax类

--- hot-deploy/partner/webapp/scmui/app/model/basedata/UnitModel.js
+++ hot-deploy/partner/webapp/scmui/app/model/basedata/UnitModel.js
@@ -2,12 +2,20 @@
 Ext.define('SCM.model.basedata.UnitModel', {
     extend: 'Ext.data.Model',
     alias: 'UnitModel',
-    //字段
-    fields: [
+    fields: [//字段
              {name: 'id',  type: 'string'},
              {name: 'number',   type: 'string'},
              {name: 'name', type: 'string'}
          ],
     requires: ['Ext.data.UuidGenerator'],
-    idgen: 'uuid' //使用uuid生成记录id 每个模型必须要有id字段
+    idgen: 'uuid', //使用uuid生成记录id 每个模型必须要有id字段
+    proxy: {
+        type: 'jsonajax',
+		api: {
+            read: '../../scm/control/requestJsonData?entity=Unit',
+            create: '../../scm/control/addnewJsonData?entity=Unit',
+            update: '../../scm/control/updateJsonData?entity=Unit',
+            destroy: '../../scm/control/deleteJsonData?entity=Unit'
+        }
+    }
 });
