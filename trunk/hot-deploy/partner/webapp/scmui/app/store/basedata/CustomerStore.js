e63751f7fa188c6177fcd09436603687392b63e4
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-01-10 13:25:29 +0000

    删除proxy定义，增加每页行数定义。

--- trunk/hot-deploy/partner/webapp/scmui/app/store/basedata/CustomerStore.js
+++ trunk/hot-deploy/partner/webapp/scmui/app/store/basedata/CustomerStore.js
@@ -1,40 +1,9 @@
-//
 Ext.define('SCM.store.basedata.CustomerStore', {
-    extend: 'Ext.data.Store',
-    model: 'SCM.model.basedata.CustomerModel',
-    alias:'CustomerStore',
-    autoLoad: false,
-    autoSync: true,
-
-    proxy: {
-        type: 'ajax',
-		api: {
-            read: '../../scm/control/requestJsonData?entity=Customer',
-            create: '../../scm/control/addnewJsonData?entity=Customer',
-            update: '../../scm/control/updateJsonData?entity=Customer',
-            destroy: '../../scm/control/deleteJsonData?entity=Customer'
-        },
-        reader: {
-            type: 'json',
-            root: 'records',
-            successProperty: 'success',
-            messageProperty: 'message'
-        },
-		writer: {
-            type: 'json',
-            root: 'records',
-            encode: true //请求服务器时以param参数传进json数据
-        },
-        listeners: {
-            exception: function(proxy, response, operation){
-                Ext.MessageBox.show({
-                    title: '服务器端异常',
-                    msg: operation.getError(),
-                    icon: Ext.MessageBox.ERROR,
-                    buttons: Ext.Msg.OK
-                });
-            }
-    }
-
-    }
-});
+			extend : 'Ext.data.Store',
+			model : 'SCM.model.basedata.CustomerModel',
+			alias : 'CustomerStore',
+			pageSize : 20, // 每页行数
+			remoteSort : true, // 服务器排序
+			autoLoad : false,
+			autoSync : true
+		});
