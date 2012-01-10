0b83538f94f62b1bf43abd510ee402e4acc563fb
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-01-10 13:24:42 +0000

    重构model，增加proxy的定义。

--- trunk/hot-deploy/partner/webapp/scmui/app/model/system/UserOfRoleModel.js
+++ trunk/hot-deploy/partner/webapp/scmui/app/model/system/UserOfRoleModel.js
@@ -4,22 +4,27 @@
  * @Date 2011-11-26
  */
 Ext.define('SCM.model.system.UserOfRoleModel', {
-    extend: 'Ext.data.Model',
-    alias: 'UserOfRoleModel',
-    fields: [
-        {name: 'id',  type: 'string'},
-        {name: 'userId',   type: 'string'},
-        {name: 'roleId',   type: 'string'}
-    ],
-    requires: ['Ext.data.UuidGenerator'],
-    idgen: 'uuid', //使用uuid生成记录id 每个模型必须要有id字段
-    proxy: {
-        type: 'jsonajax',
-        api: {
-            read: '../../scm/control/requestJsonData?entity=TSystemUserOfRole',
-            create: '../../scm/control/addnewJsonData?entity=TSystemUserOfRole',
-            update: '../../scm/control/updateJsonData?entity=TSystemUserOfRole',
-            destroy: '../../scm/control/deleteJsonData?entity=TSystemUserOfRole'
-        }
-    }
-});
+			extend : 'Ext.data.Model',
+			requires : ['Ext.data.UuidGenerator', 'SCM.extend.proxy.JsonAjax'],
+			alias : 'UserOfRoleModel',
+			fields : [{
+						name : 'id',
+						type : 'string'
+					}, {
+						name : 'userId',
+						type : 'string'
+					}, {
+						name : 'roleId',
+						type : 'string'
+					}],
+			idgen : 'uuid', //使用uuid生成记录id 每个模型必须要有id字段
+			proxy : {
+				type : 'jsonajax',
+				api : {
+					read : '../../scm/control/requestJsonData?entity=TSystemUserOfRole',
+					create : '../../scm/control/addnewJsonData?entity=TSystemUserOfRole',
+					update : '../../scm/control/updateJsonData?entity=TSystemUserOfRole',
+					destroy : '../../scm/control/deleteJsonData?entity=TSystemUserOfRole'
+				}
+			}
+		});
--- trunk/hot-deploy/partner/webapp/scmui/app/model/system/UserTreeModel.js
+++ trunk/hot-deploy/partner/webapp/scmui/app/model/system/UserTreeModel.js
@@ -4,23 +4,34 @@
  * @Date 2011-11-26
  */
 Ext.define('SCM.model.system.UserTreeModel', {
-    extend : 'Ext.data.Model',
-    alias:'UserTreeModel',
-    fields : [
-        {name: 'id',   type: 'string'},
-        {name: 'text',   type: 'string'},
-        {name: 'iconCls',   type: 'string'},
-        {name: 'leaf',   type: 'boolean', defaultValue: true},
-        {name: 'isUser',   type: 'boolean', defaultValue: true}
-    ],
-    
-    proxy: {
-        type: 'jsonajax',
-        api: {
-            read: '../../scm/control/getUserTreeToJson'
-        },
-        reader: {
-			root: 'children'
-		}
-    }
-});
+			extend : 'Ext.data.Model',
+			requires : ['SCM.extend.proxy.JsonAjax'],
+			alias : 'UserTreeModel',
+			fields : [{
+						name : 'id',
+						type : 'string'
+					}, {
+						name : 'text',
+						type : 'string'
+					}, {
+						name : 'iconCls',
+						type : 'string'
+					}, {
+						name : 'leaf',
+						type : 'boolean',
+						defaultValue : true
+					}, {
+						name : 'isUser',
+						type : 'boolean',
+						defaultValue : true
+					}],
+			proxy : {
+				type : 'jsonajax',
+				api : {
+					read : '../../scm/control/getUserTreeToJson'
+				},
+				reader : {
+					root : 'children'
+				}
+			}
+		});
