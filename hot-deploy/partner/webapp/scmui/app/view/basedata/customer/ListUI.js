fffdc4c257f9bba2f5e86529d19c85589a2e9bcd
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-01-10 13:26:02 +0000

    重构view层代码。

--- hot-deploy/partner/webapp/scmui/app/view/basedata/customer/ListUI.js
+++ hot-deploy/partner/webapp/scmui/app/view/basedata/customer/ListUI.js
@@ -1,49 +1,59 @@
 /*
- * 定义列表界面
- * Mark
+ * 定义列表界面 Mark
  */
-Ext.define('SCM.view.basedata.customer.ListUI' ,{
-    extend: 'Ext.grid.Panel',
-    alias : 'widget.custinfomaintaince',
-
-    title : '客户资料',
-    
-    store: 'basedata.CustomerStore',
-    
-    initComponent: function() {
-        this.initColumns();
-        this.initToolBar();
-        //this.initStore();
-        this.callParent(arguments);
-        this.store.load();
-    },
-
-	//初始化列
-	initColumns: function(){
-    	 this.columns = [
-    	                 {header: 'id',  dataIndex: 'id', width:200,hidden:true},
-    	                 {header: '编码',  dataIndex: 'number', width:200},
-    	                 {header: '名称', dataIndex: 'name', width:200},
-    	                 {header: '地址', dataIndex: 'address', width:200},
-    	                 {header: '联系人', dataIndex: 'contact', width:200},
-    	                 {header: '电话', dataIndex: 'phone', width:200},
-    	                 {header: '传真', dataIndex: 'fax', width:200},
-    	                 {header: '邮政编码', dataIndex: 'postCode', width:200}
-    	 ];
-    },
-    //初始化工具栏
-    initToolBar: function(){
-    	this.dockedItems=[
-    	   {xtype:'toolbar',
-    		items:[{text:'新增',cls:'x-btn-text-icon',icon:'/scmui/images/icons/add.png',action:'addNew'},
-    		       {text:'修改',cls:'x-btn-text-icon',icon:'/scmui/images/icons/edit.png',action:'modify'},
-    		       {text:'删除',cls:'x-btn-text-icon',icon:'/scmui/images/icons/delete.png',action:'delete'},
-    		       {text:'刷新',cls:'x-btn-text-icon',icon:'/scmui/images/icons/refresh.png',action:'refresh'},
-    		       '->',{xtype:'textfield'},{text:'查询'}]}
-    	];
-    },
-    //初始化数据
-    initStore: function(){
-    	
-    }
-});
+Ext.define('SCM.view.basedata.customer.ListUI', {
+			extend : 'Ext.grid.Panel',
+			requires : ['SCM.extend.toolbar.BaseToolbar'],
+			alias : 'widget.custinfomaintaince',
+			title : '客户资料',
+			store : 'basedata.CustomerStore',
+			initComponent : function() {
+				var me = this;
+				Ext.applyIf(me, {
+							columns : [{
+										header : 'id',
+										dataIndex : 'id',
+										width : 100,
+										hidden : true
+									}, {
+										header : '编码',
+										dataIndex : 'number',
+										width : 100
+									}, {
+										header : '名称',
+										dataIndex : 'name',
+										width : 200
+									}, {
+										header : '地址',
+										dataIndex : 'address',
+										width : 200
+									}, {
+										header : '联系人',
+										dataIndex : 'contractor',
+										width : 100
+									}, {
+										header : '电话',
+										dataIndex : 'phone',
+										width : 100
+									}, {
+										header : '传真',
+										dataIndex : 'fax',
+										width : 100
+									}, {
+										header : '邮政编码',
+										dataIndex : 'postCode',
+										width : 100
+									}],
+							dockedItems : [{
+										xtype : 'basetoolbar'
+									}, {
+										dock : 'bottom',
+										xtype : 'pagingtoolbar',
+										store : 'basedata.CustomerStore',
+										displayInfo : true
+									}]
+						});
+				this.callParent();
+				this.store.loadPage(1);
+			}
+		});
--- hot-deploy/partner/webapp/scmui/app/view/basedata/unit/ListUI.js
+++ hot-deploy/partner/webapp/scmui/app/view/basedata/unit/ListUI.js
@@ -3,71 +3,37 @@
  */
 Ext.define('SCM.view.basedata.unit.ListUI', {
 			extend : 'Ext.grid.Panel',
+			requires : ['SCM.extend.toolbar.BaseToolbar'],
 			alias : 'widget.unitlist',
 			title : '计量单位',
 			store : 'basedata.UnitStore',
 			initComponent : function() {
-				this.initColumns();
-				this.initToolBar();
-				this.callParent(arguments);
-				this.store.loadPage(1);
-			},
-
-			// 初始化列
-			initColumns : function() {
-				this.columns = [{
-							header : 'id',
-							dataIndex : 'id',
-							width : 200,
-							hidden : true
-						}, {
-							header : '编码',
-							dataIndex : 'number',
-							width : 200
-						}, {
-							header : '名称',
-							dataIndex : 'name',
-							width : 200
-						}];
-			},
-			// 初始化工具栏
-			initToolBar : function() {
-				this.dockedItems = [{
-							xtype : 'toolbar',
-							height: 28,
-							items : [{
-										xtype : 'textfield',
-										name : 'keyWord',
-										emptyText : '请输入单位名称',
-										id : 'keyWord'
+				var me = this;
+				Ext.applyIf(me, {
+							columns : [{
+										header : 'id',
+										dataIndex : 'id',
+										width : 200,
+										hidden : true
 									}, {
-										text : '查询',
-										iconCls : 'system-search',
-										action : 'search'
+										header : '编码',
+										dataIndex : 'number',
+										width : 200
 									}, {
-										text : '新增',
-										iconCls : 'system-add',
-										action : 'addNew'
+										header : '名称',
+										dataIndex : 'name',
+										width : 200
+									}],
+							dockedItems : [{
+										xtype : 'basetoolbar'
 									}, {
-										text : '修改',
-										iconCls : 'system-edit',
-										action : 'modify'
-									}, {
-										text : '删除',
-										iconCls : 'system-delete',
-										action : 'delete'
-									}, {
-										text : '导出',
-										iconCls : 'system-export',
-										action : 'export'
+										dock : 'bottom',
+										xtype : 'pagingtoolbar',
+										store : 'basedata.UnitStore',
+										displayInfo : true
 									}]
-						}, {
-							dock : 'bottom',
-							xtype : 'pagingtoolbar',
-							store : 'basedata.UnitStore',
-							displayInfo : true,
-							displayMsg : '显示 {0} - {1} 条，共计 {2} 条',
-							emptyMsg : '没有数据'
-						}];
+						});
+				this.callParent();
+				this.store.loadPage(1);
 			}
 		});
