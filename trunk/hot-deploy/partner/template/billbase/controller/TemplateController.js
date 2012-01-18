734c41e36fbd6bc05467f96cc233aa606cb5c97a
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-01-18 08:14:22 +0000

    增加列表双击、重置、取消按钮事件。

--- trunk/hot-deploy/partner/template/billbase/controller/TemplateController.js
+++ trunk/hot-deploy/partner/template/billbase/controller/TemplateController.js
@@ -1,79 +1,84 @@
 Ext.define('SCM.controller.${TemplateName}.${TemplateName}Controller', {
-    extend: 'Ext.app.Controller',
-    mixins:['SCM.extend.controller.BillCommonController'],
-	views: [
-        '${TemplateName}.ListUI',
-		'${TemplateName}.EditUI'
-    ],
-	
-	stores:[
-		'${TemplateName}.${TemplateName}Store',
-		'${TemplateName}.${TemplateName}EditStore',
-		'${TemplateName}.${TemplateName}EditEntryStore'
-	],
-	requires:['SCM.model.${TemplateName}.${TemplateName}ActionModel'],
-	editName:'${TemplateName}edit',
-	editStoreName:'${TemplateName}EditStore',
-	entityName:'${TemplateName}',
-	modelName:'${TemplateName}EditModel',
-	entryModelName:'${TemplateName}EditEntryModel',
-	actionModelName:'${TemplateName}ActionModel',
-    init: function() {
-		this.control({
-			'${TemplateName}list':{
-	        	afterrender: this.initComponent
-	        },
-			//列表新增按钮
-	        '${TemplateName}list button[action=addNew]':{
-	        	click: this.addNewRecord
-	        },
-			//列表事件
-	        '${TemplateName}list gridpanel[region=center]': {
-	    		select: this.showDetail //列表选择事件，显示明细
-	        },
-	        //列表修改按钮
-	        '${TemplateName}list button[action=modify]':{
-	        	click: this.editRecord
-	        },
-	        //列表删除按钮
-	        '${TemplateName}list button[action=delete]':{
-	        	click: this.deleteRecord
-	        },
-			//列表界面刷新
-			'${TemplateName}list button[action=refresh]':{
-	        	click: this.refreshRecord
-	        },
-	        //列表审核按钮
-	        '${TemplateName}list button[action=audit]':{
-	        	click: this.auditBill
-	        },
-	        //列表反审核按钮
-	        '${TemplateName}list button[action=unaudit]':{
-	        	click: this.unauditBill
-	        },
-	        //列表打印按钮
-	        '${TemplateName}list button[action=print]':{
-	        	click: this.print
-	        },
-			//编辑界面分录新增
-	        '${TemplateName}edit  gridpanel button[action=addLine]':{
-	        	click: this.addLine
-	        },
-			//编辑界面分录删除
-	        '${TemplateName}edit  gridpanel button[action=deleteLine]':{
-	        	click: this.deleteLine
-	        },
-			
-			//编辑界面保存
-			'${TemplateName}edit button[action=save]':{
-				click: this.saveRecord
-			},
-			//编辑界面打印
-			'${TemplateName}edit button[action=print]':{
-				click: this.print
-			}
-		}
-		);
-    }
+			extend : 'Ext.app.Controller',
+			mixins : ['SCM.extend.controller.BillCommonController'],
+			views : ['${TemplateName}.ListUI', '${TemplateName}.EditUI'],
+			stores : ['${TemplateName}.${TemplateName}Store', '${TemplateName}.${TemplateName}EditStore', '${TemplateName}.${TemplateName}EditEntryStore'],
+			requires : ['SCM.model.${TemplateName}.${TemplateName}ActionModel'],
+			editName : '${TemplateName}edit',
+			editStoreName : '${TemplateName}EditStore',
+			entityName : '${TemplateName}',
+			modelName : '${TemplateName}EditModel',
+			entryModelName : '${TemplateName}EditEntryModel',
+			actionModelName : '${TemplateName}ActionModel',
+			init : function() {
+				this.control({
+							'${TemplateName}list' : {
+								afterrender : this.initComponent
+							},
+							// 列表事件
+							'${TemplateName}list gridpanel' : {
+								itemdblclick : this.modifyRecord, // 双击列表，弹出编辑界面
+								itemclick : this.changeComponentsState // 点击列表，改变修改、删除按钮状态
+							},
+							// 列表新增按钮
+							'${TemplateName}list button[action=addNew]' : {
+								click : this.addNewRecord
+							},
+							// 列表事件
+							'${TemplateName}list gridpanel[region=center]' : {
+								select : this.showDetail
+								// 列表选择事件，显示明细
+							},
+							// 列表修改按钮
+							'${TemplateName}list button[action=modify]' : {
+								click : this.editRecord
+							},
+							// 列表删除按钮
+							'${TemplateName}list button[action=delete]' : {
+								click : this.deleteRecord
+							},
+							// 列表界面刷新
+							'${TemplateName}list button[action=refresh]' : {
+								click : this.refreshRecord
+							},
+							// 列表审核按钮
+							'${TemplateName}list button[action=audit]' : {
+								click : this.auditBill
+							},
+							// 列表反审核按钮
+							'${TemplateName}list button[action=unaudit]' : {
+								click : this.unauditBill
+							},
+							// 列表打印按钮
+							'${TemplateName}list button[action=print]' : {
+								click : this.print
+							},
+							// 编辑界面分录新增
+							'${TemplateName}edit  gridpanel button[action=addLine]' : {
+								click : this.addLine
+							},
+							// 编辑界面分录删除
+							'${TemplateName}edit  gridpanel button[action=deleteLine]' : {
+								click : this.deleteLine
+							},
 
-});
+							// 编辑界面保存
+							'${TemplateName}edit button[action=save]' : {
+								click : this.saveRecord
+							},
+							// 编辑界面打印
+							'${TemplateName}edit button[action=print]' : {
+								click : this.print
+							},
+							// 编辑界面重填
+							'${TemplateName}edit button[action=clear]' : {
+								click : this.clear
+							},
+							// 编辑界面取消
+							'${TemplateName}edit button[action=cancel]' : {
+								click : this.cancel
+							}
+						});
+			}
+
+		});
