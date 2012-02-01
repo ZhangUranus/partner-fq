f3bc3c2a220a22a969555893dde0ae11789748d8
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-02-01 09:32:00 +0000

    增加采购退货单据页面

---
+++ hot-deploy/partner/webapp/scmui/app/model/PurchaseReturn/PurchaseReturnModel.js
@@ -0,0 +1,90 @@
+// 定义数据模型
+Ext.define('SCM.model.PurchaseReturn.PurchaseReturnModel', {
+			extend : 'Ext.data.Model',
+			requires : ['SCM.extend.proxy.JsonAjax'],
+			alias : 'PurchaseReturnModel',
+			// 字段
+			fields : [{
+						name : 'id',
+						type : 'string'
+					}, {
+						name : 'emptyId',
+						type : 'string',
+						persist : false
+					}, {
+						name : 'number',
+						type : 'string'
+					}, {
+						name : 'bizDate',
+						type : 'date',
+						dateFormat : 'time'
+					}, {
+						name : 'supplierSupplierId',
+						type : 'string'
+					}, {
+						name : 'supplierSupplierName',
+						type : 'string',
+						persist : false
+					}, {
+						name : 'returnerSystemUserId',
+						type : 'string'
+					}, {
+						name : 'returnerSystemUserName',
+						type : 'string',
+						persist : false
+					}, {
+						name : 'totalsum',
+						type : 'float'
+					}, {
+						name : 'entryId',
+						type : 'string'
+					}, {
+						name : 'warehouseWarehouseId',
+						type : 'string'
+					}, {
+						name : 'warehouseWarehouseName',
+						type : 'string',
+						persist : false
+					}, {
+						name : 'materialMaterialId',
+						type : 'string'
+					}, {
+						name : 'materialMaterialName',
+						type : 'string',
+						persist : false
+					}, {
+						name : 'materialMaterialModel',
+						type : 'string'
+					}, {
+						name : 'volume',
+						type : 'float'
+					}, {
+						name : 'stockVolume',
+						type : 'float'
+					}, {
+						name : 'unitUnitId',
+						type : 'string'
+					}, {
+						name : 'unitUnitName',
+						type : 'string',
+						persist : false
+					}, {
+						name : 'price',
+						type : 'float'
+					}, {
+						name : 'refPrice',
+						type : 'float'
+					}, {
+						name : 'entrysum',
+						type : 'float'
+					}],
+			// ,idProperty:'emptyId'//设置一个没用的id，这样才能支持显示多分录
+			proxy : {
+				type : 'jsonajax',
+				api : {
+					read : '../../scm/control/requestJsonData?entity=PurchaseReturnView',
+					destroy : '../../scm/control/deleteWithEntry?headEntity=PurchaseReturn&entryEntity=PurchaseReturnEntry'
+				},
+				remoteFilter : true
+			}
+		});
---
+++ hot-deploy/partner/webapp/scmui/app/store/PurchaseReturn/PurchaseReturnEditStore.js
@@ -0,0 +1,7 @@
+Ext.define('SCM.store.PurchaseReturn.PurchaseReturnEditStore', {
+			extend : 'Ext.data.Store',
+			model : 'SCM.model.PurchaseReturn.PurchaseReturnEditModel',
+			alias : 'PurchaseReturnEditStore',
+			autoLoad : false,
+			autoSync : false
+		});
