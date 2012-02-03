664a845151947c0eab534b774882246be51ee21d
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-02-03 09:04:01 +0000

    增加制造退料页面。

---
+++ trunk/hot-deploy/partner/webapp/scmui/app/view/WorkshopReturnMaterial/ListUI.js
@@ -0,0 +1,140 @@
+/*
+ * 定义物料Bom列表界面 Mark
+ */
+Ext.define('SCM.view.WorkshopReturnMaterial.ListUI', {
+			extend : 'Ext.container.Container',
+			requires : ['SCM.extend.toolbar.BillBaseToolbar'],
+			alias : 'widget.WorkshopReturnMateriallist',
+			title : '制造退料单查询',
+			height : 497,
+			width : 718,
+			layout : {
+				type : 'border'
+			},
+			initComponent : function() {
+				var me = this;
+				var entryStore = Ext.create('WorkshopReturnMaterialEditEntryStore', {
+							id : 'WorkshopReturnMaterialListEntry'
+						});
+				Ext.applyIf(me, {
+							items : [{
+										xtype : 'billbasetoolbar',// 工具栏
+										audit : true,
+										custType : 'workshop',
+										region : 'north'
+									}, {
+										xtype : 'gridpanel',
+										margin : '1 0 0 0',
+										title : '',
+										region : 'center',
+										store : 'WorkshopReturnMaterial.WorkshopReturnMaterialEditStore',
+										columns : [{
+													header : '序号',
+													xtype : 'rownumberer',
+													width : 40
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'id',
+													text : 'id',
+													hidden : true
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'number',
+													width : 150,
+													text : '编码'
+												}, {
+													xtype : 'datecolumn',
+													dataIndex : 'bizDate',
+													width : 120,
+													format : 'Y-m-d',
+													groupable : false,
+													text : '业务日期'
+												}, {
+													xtype : 'gridcolumn',
+													renderer : SCM.store.basiccode.billStatusRenderer,
+													dataIndex : 'status',
+													width : 80,
+													groupable : false,
+													text : '单据状态'
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'workshopWorkshopName',
+													width : 150,
+													groupable : false,
+													text : '车间'
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'checkerSystemUserName',
+													width : 80,
+													groupable : false,
+													text : '验收员'
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'approverSystemUserName',
+													width : 80,
+													groupable : false,
+													text : '审核员'
+												}, {
+													xtype : 'numbercolumn',
+													dataIndex : 'totalsum',
+													width : 100,
+													groupable : false,
+													text : '总金额'
+												}],
+										viewConfig : {}
+									}, {
+										xtype : 'gridpanel',
+										title : '',
+										region : 'south',
+										height : 150,
+										split : true,
+										store : entryStore,
+										columns : [{
+													header : '序号',
+													xtype : 'rownumberer',
+													width : 40
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'id',
+													text : 'id',
+													hidden : true
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'parentId',
+													text : 'parentId',
+													hidden : true
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'warehouseWarehouseName',
+													text : '仓库'
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'materialMaterialName',
+													text : '物料'
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'materialMaterialModel',
+													text : '规格型号'
+												}, {
+													xtype : 'numbercolumn',
+													dataIndex : 'volume',
+													text : '数量'
+												}, {
+													xtype : 'gridcolumn',
+													dataIndex : 'unitUnitName',
+													text : '单位'
+												}, {
+													xtype : 'numbercolumn',
+													dataIndex : 'price',
+													text : '单价'
+												}, {
+													xtype : 'numbercolumn',
+													dataIndex : 'entrysum',
+													text : '金额'
+												}]
+									}]
+						});
+				me.callParent(arguments);
+				// me.down('gridpanel').store.load();
+			}
+		});
