b7772d83799913a9ed022314b130d3a8a0c0617e
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-02-03 08:59:31 +0000

    增加页面

--- hot-deploy/partner/webapp/scmui/app.js
+++ hot-deploy/partner/webapp/scmui/app.js
@@ -15,9 +15,9 @@ Ext.application({
 			appFolder : 'app',
 			controllers : [// 载入系统controller
 			'Main', 'basedata.UnitController', 'basedata.WarehouseTypeController', 'basedata.WarehouseController', 'basedata.CustomerController', 'basedata.DepartmentController',
-					'basedata.MaterialController', 'basedata.MaterialBomController', 'PurchaseBill.PurchaseBillController', 'Supplier.SupplierController', 'system.SystemController',
+					'basedata.MaterialController', 'basedata.MaterialBomController', 'basedata.WorkshopController', 'PurchaseBill.PurchaseBillController', 'Supplier.SupplierController', 'system.SystemController',
 					'PurchaseWarehousing.PurchaseWarehousingController', 'PurchaseReturn.PurchaseReturnController', 'ConsignDrawMaterial.ConsignDrawMaterialController',
-					'ConsignReturnMaterial.ConsignReturnMaterialController'],
+					'ConsignReturnMaterial.ConsignReturnMaterialController', 'WorkshopDrawMaterial.WorkshopDrawMaterialController', 'WorkshopReturnMaterial.WorkshopReturnMaterialController'],
 			launch : function() {
 				var viewport = Ext.create('SCM.view.Viewport');
 				viewport.doLayout(); // 刷新布局
