e470c5331b37df40ab8d0f87bcfa63cf1b99cd47
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-02-06 06:10:16 +0000

    增加委外入库和制造入库。

---
+++ hot-deploy/partner/webapp/scmui/app/store/ConsignWarehousing/ConsignWarehousingEditStore.js
@@ -0,0 +1,7 @@
+Ext.define('SCM.store.ConsignWarehousing.ConsignWarehousingEditStore', {
+			extend : 'Ext.data.Store',
+			model : 'SCM.model.ConsignWarehousing.ConsignWarehousingEditModel',
+			alias : 'ConsignWarehousingEditStore',
+			autoLoad : false,
+			autoSync : false
+		});
---
+++ hot-deploy/partner/webapp/scmui/app/store/WorkshopWarehousing/WorkshopWarehousingStore.js
@@ -0,0 +1,8 @@
+Ext.define('SCM.store.WorkshopWarehousing.WorkshopWarehousingStore', {
+			extend : 'Ext.data.Store',
+			model : 'SCM.model.WorkshopWarehousing.WorkshopWarehousingModel',
+			alias : 'WorkshopWarehousingStore',
+			autoLoad : false,
+			autoSync : false,
+			groupField : 'number'
+		});
