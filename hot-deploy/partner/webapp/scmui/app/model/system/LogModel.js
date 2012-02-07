d52f7a951d050f7b7c50b9c2f482111d2e28da1a
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-02-07 13:42:46 +0000

    增加日志页面

---
+++ hot-deploy/partner/webapp/scmui/app/model/system/LogModel.js
@@ -0,0 +1,49 @@
+/**
+ * @Purpose 角色model
+ * @author jeff-liu
+ * @Date 2011-11-26
+ */
+Ext.define('SCM.model.system.LogModel', {
+			extend : 'Ext.data.Model',
+			requires : ['Ext.data.UuidGenerator', 'SCM.extend.proxy.JsonAjax'],
+			alias : 'LogModel',
+			fields : [{
+						name : 'id',
+						type : 'string'
+					}, {
+						name : 'hitTime',
+						type : 'date',
+						dateFormat : 'time'
+					}, {
+						name : 'hitType',
+						type : 'string'
+					}, {
+						name : 'runningTime',
+						type : 'string'
+					}, {
+						name : 'userId',
+						type : 'string'
+					}, {
+						name : 'url',
+						type : 'string'
+					}, {
+						name : 'ipAddress',
+						type : 'string'
+					}, {
+						name : 'hostName',
+						type : 'string'
+					}, {
+						name : 'name',
+						type : 'string'
+					}, {
+						name : 'operateType',
+						type : 'string'
+					}],
+			idgen : 'uuid', // 使用uuid生成记录id 每个模型必须要有id字段
+			proxy : {
+				type : 'jsonajax',
+				api : {
+					read : '../../scm/control/requestJsonData?entity=VSystemLog'
+				}
+			}
+		});
