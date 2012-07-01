7259708af5295da5ef5d5b15905413db7b193dfd
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-07-01 09:03:34 +0000

    增加仓库调整单、车间调整单、供应商调整单。

---
+++ hot-deploy/partner/src/org/ofbiz/partner/scm/stock/StockAdjustEvents.java
@@ -0,0 +1,102 @@
+package org.ofbiz.partner.scm.stock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.transaction.GenericTransactionException;
+import org.ofbiz.entity.transaction.TransactionUtil;
+import org.ofbiz.partner.scm.common.BillBaseEvent;
+import org.ofbiz.partner.scm.pricemgr.BillType;
+import org.ofbiz.partner.scm.pricemgr.BizStockImpFactory;
+/**
+ * 仓库调整单事件类
+ * 
+ * @author Mark
+ * 
+ */
+public class StockAdjustEvents {
+	private static final String module = org.ofbiz.partner.scm.stock.StockAdjustEvents.class.getName();
+
+	/**
+	 * 仓库调整单提交
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public static String submitBill(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		boolean beganTransaction = false;
+		try {
+			beganTransaction = TransactionUtil.begin();
+
+			Delegator delegator = (Delegator) request.getAttribute("delegator");
+			String billId = request.getParameter("billId");// 单据id
+			if (delegator != null && billId != null) {
+				Debug.log("仓库调整单提交:" + billId, module);
+				GenericValue billHead = delegator.findOne("StockAdjust", UtilMisc.toMap("id", billId), false);
+				if (billHead == null || billHead.get("bizDate") == null) {
+					throw new Exception("can`t find StockAdjust bill or bizdate is null");
+				}
+
+				BizStockImpFactory.getBizStockImp(BillType.StockAdjust).updateStock(billHead, false, false);
+
+				BillBaseEvent.submitBill(request, response);// 更新单据状态
+			}
+			TransactionUtil.commit(beganTransaction);
+		} catch (Exception e) {
+			Debug.logError(e, module);
+			try {
+				TransactionUtil.rollback(beganTransaction, e.getMessage(), e);
+			} catch (GenericTransactionException e2) {
+				Debug.logError(e2, "Unable to rollback transaction", module);
+			}
+			throw e;
+		}
+		return "success";
+	}
+
+	/**
+	 * 仓库调整单撤销
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public static String rollbackBill(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		boolean beganTransaction = false;
+		try {
+			beganTransaction = TransactionUtil.begin();
+
+			Delegator delegator = (Delegator) request.getAttribute("delegator");
+			String billId = request.getParameter("billId");// 单据id
+			if (delegator != null && billId != null) {
+				Debug.log("仓库调整单撤销:" + billId, module);
+				GenericValue billHead = delegator.findOne("StockAdjust", UtilMisc.toMap("id", billId), false);
+				if (billHead == null || billHead.get("bizDate") == null) {
+					throw new Exception("can`t find StockAdjust bill or bizdate is null");
+				}
+
+				BizStockImpFactory.getBizStockImp(BillType.StockAdjust).updateStock(billHead, true, true);
+
+				BillBaseEvent.rollbackBill(request, response);// 撤销单据
+			}
+			TransactionUtil.commit(beganTransaction);
+		} catch (Exception e) {
+			Debug.logError(e, module);
+			try {
+				TransactionUtil.rollback(beganTransaction, e.getMessage(), e);
+			} catch (GenericTransactionException e2) {
+				Debug.logError(e2, "Unable to rollback transaction", module);
+			}
+			throw e;
+		}
+		return "success";
+	}
+	
+}
