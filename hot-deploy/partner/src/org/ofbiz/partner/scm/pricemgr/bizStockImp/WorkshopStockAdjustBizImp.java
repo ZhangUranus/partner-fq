2e0f649a4ae5affbadece1c3f6f3212e9939e086
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-08-31 01:11:11 +0000

    修正代码，将物料查询改为模糊查询。具体见partner\hot-deploy\partner\document\测试\系统测试情况-刘继煌.xlsx

--- hot-deploy/partner/src/org/ofbiz/partner/scm/pricemgr/bizStockImp/WorkshopStockAdjustBizImp.java
+++ hot-deploy/partner/src/org/ofbiz/partner/scm/pricemgr/bizStockImp/WorkshopStockAdjustBizImp.java
@@ -1,24 +1,19 @@
 package org.ofbiz.partner.scm.pricemgr.bizStockImp;
 
 import java.math.BigDecimal;
-import java.util.Date;
 import java.util.List;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericValue;
-import org.ofbiz.partner.scm.pricemgr.BillType;
 import org.ofbiz.partner.scm.pricemgr.IBizStock;
-import org.ofbiz.partner.scm.pricemgr.PriceCalItem;
-import org.ofbiz.partner.scm.pricemgr.PriceMgr;
 import org.ofbiz.partner.scm.pricemgr.WorkshopPriceMgr;
 
 public class WorkshopStockAdjustBizImp implements IBizStock {
 	private Delegator delegator = org.ofbiz.partner.scm.common.Utils.getDefaultDelegator();
 
 	public synchronized void updateStock(GenericValue billValue, boolean isOut, boolean isCancel) throws Exception {
-		Date bizDate = (Date) billValue.get("bizDate");
 
 		// 获取单据id分录条目
 		List<GenericValue> entryList = delegator.findByAnd("WorkshopStockAdjustEntry", UtilMisc.toMap("parentId", billValue.getString("id")));
@@ -36,15 +31,18 @@ public class WorkshopStockAdjustBizImp implements IBizStock {
 			String workshopId = v.getString("workshopWorkshopId");// 仓库id
 			String materialId = v.getString("materialMaterialId");// 物料id
 			BigDecimal volume = v.getBigDecimal("volume");// 数量
-			BigDecimal price = WorkshopPriceMgr.getInstance().getPrice(workshopId, materialId);
-			if(price == BigDecimal.ZERO){
-				price = v.getBigDecimal("price");
-			}
+			//2012-08-30 不允许用户输入单价，调整单没有单价
+//			BigDecimal price = WorkshopPriceMgr.getInstance().getPrice(workshopId, materialId);
+//			if(price == BigDecimal.ZERO){
+//				price = v.getBigDecimal("price");
+//			}
+			BigDecimal price = BigDecimal.ZERO;
 			
 			if(volume.compareTo(BigDecimal.ZERO)<=0){
 				throw new Exception("车间调整单入库物料数量不能小于等于零，请重新输入！");
 			}
-			BigDecimal sum = volume.multiply(price);// 金额
+//			BigDecimal sum = volume.multiply(price);// 金额
+			BigDecimal sum = BigDecimal.ZERO;
 			Debug.log("车间调整单入库单价计算:物料id" + materialId + ";数量" + volume + ";金额" + sum, "WorkshopStockAdjustBizImp");
 			
 			v.set("price", price);
