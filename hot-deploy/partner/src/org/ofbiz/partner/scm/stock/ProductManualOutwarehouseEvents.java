eb97441134d8618efdeafb181d7eb80970e5b1d9
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2012-10-23 14:44:11 +0000

    增加成品手工出仓功能。

---
+++ hot-deploy/partner/src/org/ofbiz/partner/scm/stock/ProductManualOutwarehouseEvents.java
@@ -0,0 +1,116 @@
+package org.ofbiz.partner.scm.stock;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.transaction.GenericTransactionException;
+import org.ofbiz.entity.transaction.TransactionUtil;
+import org.ofbiz.partner.scm.common.BillBaseEvent;
+import org.ofbiz.partner.scm.pricemgr.BillType;
+import org.ofbiz.partner.scm.pricemgr.BizStockImpFactory;
+import org.ofbiz.partner.scm.pricemgr.Utils;
+
+/**
+ * 验收单业务事件类
+ * 
+ * @author Mark
+ * 
+ */
+public class ProductManualOutwarehouseEvents {
+	private static final String module = org.ofbiz.partner.scm.stock.ProductManualOutwarehouseEvents.class.getName();
+
+	/**
+	 * 成品手工出仓单提交
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public static String submitBill(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		boolean beganTransaction = false;
+		try {
+			beganTransaction = TransactionUtil.begin();
+
+			Delegator delegator = (Delegator) request.getAttribute("delegator");
+			String billId = request.getParameter("billId");// 单据id
+			if (delegator != null && billId != null) {
+				Debug.log("成品手工出仓单审核:" + billId, module);
+				GenericValue billHead = delegator.findOne("ProductManualOutwarehouse", UtilMisc.toMap("id", billId), false);
+				if (billHead == null || billHead.get("bizDate") == null) {
+					throw new Exception("can`t find ProductManualOutwarehouse bill or bizdate is null");
+				}
+				// 注意不能使用billHead.getDate方法，出产生castException异常
+				Date bizDate = (Date) billHead.get("bizDate");
+				if (bizDate == null || !Utils.isCurPeriod(bizDate)) {
+					throw new Exception("单据业务日期不在当前系统期间");
+				}
+
+				BizStockImpFactory.getBizStockImp(BillType.ProductManualOutwarehouse).updateStock(billHead, true, false);
+
+				BillBaseEvent.submitBill(request, response);// 更新单据状态
+
+			}
+			TransactionUtil.commit(beganTransaction);
+		} catch (Exception e) {
+			Debug.logError(e, module);
+			try {
+				TransactionUtil.rollback(beganTransaction, e.getMessage(), e);
+			} catch (GenericTransactionException e2) {
+				Debug.logError(e2, "Unable to rollback transaction", module);
+			}
+			throw e;
+		}
+		return "success";
+	}
+
+	/**
+	 * 采购退货单撤销
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public static String rollbackBill(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		boolean beganTransaction = false;
+		try {
+			beganTransaction = TransactionUtil.begin();
+
+			Delegator delegator = (Delegator) request.getAttribute("delegator");
+			String billId = request.getParameter("billId");// 单据id
+			if (delegator != null && billId != null) {
+				Debug.log("成品手工出仓单撤销:" + billId, module);
+				GenericValue billHead = delegator.findOne("ProductManualOutwarehouse", UtilMisc.toMap("id", billId), false);
+				if (billHead == null || billHead.get("bizDate") == null) {
+					throw new Exception("can`t find ProductManualOutwarehouse bill or bizdate is null");
+				}
+				// 注意不能使用billHead.getDate方法，出产生castException异常
+				Date bizDate = (Date) billHead.get("bizDate");
+				if (bizDate == null || !Utils.isCurPeriod(bizDate)) {
+					throw new Exception("单据业务日期不在当前系统期间");
+				}
+
+				BizStockImpFactory.getBizStockImp(BillType.ProductManualOutwarehouse).updateStock(billHead, false, true);
+
+				BillBaseEvent.rollbackBill(request, response);// 撤销单据
+			}
+			TransactionUtil.commit(beganTransaction);
+		} catch (Exception e) {
+			Debug.logError(e, module);
+			try {
+				TransactionUtil.rollback(beganTransaction, e.getMessage(), e);
+			} catch (GenericTransactionException e2) {
+				Debug.logError(e2, "Unable to rollback transaction", module);
+			}
+			throw e;
+		}
+		return "success";
+	}
+}
