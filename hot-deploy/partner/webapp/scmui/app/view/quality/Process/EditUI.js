cdcbe22dd4958f50f1e2b29ddc3fc3b333b2f425
liujihuang@gmail.com <liujihuang@gmail.com@af26d51f-1f06-fef7-9697-33d4cfc65487>
2013-05-14 13:38:25 +0000

    工序表维护功能。

---
+++ hot-deploy/partner/webapp/scmui/app/view/quality/Process/EditUI.js
@@ -0,0 +1,62 @@
+Ext.define('SCM.view.quality.Process.EditUI', {
+			extend : 'Ext.window.Window',
+			requires : ['SCM.extend.toolbar.SaveToolbar'],
+			alias : 'widget.processedit',
+			title : '工序',
+			layout : 'fit',
+			width : SCM.DefaultSize.WINDOW_WIDTH,
+			modal : true,// 背景变灰，不能编辑
+			collapsible : true,
+			resizable : false,
+			closeAction : 'hide',
+			uiStatus : 'AddNew',
+			inited : false, // 初始化标识
+			modifyed : false, // 修改标识
+
+			initComponent : function() {
+				var me = this;
+				Ext.applyIf(me, {
+							items : [{
+										xtype : 'form',
+										bodyPadding : '10 10 10 10',
+										border : 0,
+										defaults : {
+											xtype : 'textfield',
+											labelWidth : SCM.DefaultSize.LABEL_WIDTH,
+											width : SCM.DefaultSize.FIELD_WIDTH
+										},
+										items : [{
+													name : 'id',
+													fieldLabel : 'id',
+													hidden : true
+												}, {
+													name : 'number',
+													fieldLabel : '编码',
+													emptyText : '保存时系统自动生成',
+													readOnly : true
+												}, {
+													name : 'name',
+													fieldLabel : '名称',
+													allowBlank : false,
+													maxLength : 50
+												}, {
+													xtype : 'textarea',
+													name : 'description',
+													fieldLabel : '描述',
+													maxLength : 50
+												}]
+									}],
+							dockedItems : [{
+										xtype : 'savetoolbar',
+										dock : 'bottom'
+									}]
+						});
+				this.callParent();
+			},
+
+			close : function() {
+				this.hide();
+				this.inited = false;
+				this.modifyed = false;
+			}
+		});
